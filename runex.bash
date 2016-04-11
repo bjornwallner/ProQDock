0d84b3bfb0400a52ba6537c45068034c773a3f73
nemo8130 <nemo8130@gmail.com>
2016-04-11 13:29:04 +0200

    README-updated

--- runex.bash
+++ runex.bash
@@ -2,7 +2,7 @@
 
 for i in `ls ../ProQDock/EXAMPLE_CASE_STUDY/D1OYV*-delpatch.pdb`
 do
-echo "./ProQDock.bash $i ./EXAMPLE_CASE_STUDY/1OYV.pdb.fasta"
-./ProQDock.bash $i ./EXAMPLE_CASE_STUDY/1OYV.pdb.fasta
+echo "./ProQDock $i ./EXAMPLE_CASE_STUDY/1OYV.pdb.fasta"
+./ProQDock $i ./EXAMPLE_CASE_STUDY/1OYV.pdb.fasta
 done
 
